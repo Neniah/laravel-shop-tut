ec6d548ea90ea7803e14527b0057f4ffa4c5060b
Maria Lobillo <maria.lobillo.santos@gmail.com>
2016-10-09 18:27:32 +0200

    routes!!

--- routes/web.php
+++ routes/web.php
@@ -1,1 +1,15 @@
 <?php
+
+Route::get('/', 'Front@index');
+Route::get('/products', 'Front@product');
+Route::get('/products/details/{id}', 'Front@product_details');
+Route::get('/products/categories', 'Front@product_categories');
+Route::get('/products/brands', 'Front@product_brands');
+Route::get('/blog', 'Front@blog');
+Route::get('/blog/post/{id}', 'Front@blog_post');
+Route::get('/contact-us', 'Front@contact_us');
+Route::get('/login', 'Front@login');
+Route::get('/logout', 'Front@logout');
+Route::get('/cart', 'Front@cart');
+Route::get('/checkout', 'Front@cart');
+Route::get('/search/{query}', 'Front@search');
