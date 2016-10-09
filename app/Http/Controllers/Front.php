e6fb628f1508ab6aab9e1e4baf8bdda2c42b5db4
Maria Lobillo <maria.lobillo.santos@gmail.com>
2016-10-09 18:20:18 +0200

    methods for Front Controller

--- app/Http/Controllers/Front.php
+++ app/Http/Controllers/Front.php
@@ -9,4 +9,56 @@ use App\Http\Requests;
 class Front extends Controller
 {
     //
+    public function index(){
+      return 'index page';
+    }
+
+    public function product(){
+      return 'product page';
+    }
+
+    public function product_details($id){
+      return '$id product details page';
+    }
+
+    public function product_categories(){
+      return 'product categories page';
+    }
+
+    public function product_brands(){
+      return 'product brands page';
+    }
+
+    public function blog(){
+      return 'blog page';
+    }
+
+    public function blog_post($id){
+      return '$id blog post page';
+    }
+
+    public function contact_us(){
+      return 'contact us page';
+    }
+
+    public function login(){
+      return 'login page';
+    }
+
+    public function logout(){
+      return 'logout page';
+    }
+
+    public function cart(){
+      return 'cart page';
+    }
+
+    public function checkout(){
+      return 'checkout page';
+    }
+
+    public function search($query){
+      return '$query search page';
+    }
+
 }
--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,1 @@
 <?php
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/hello', function(){
-  return 'Hello World!!';
-});
-
-Route::get('hello', 'Hello@index');
-
-Route::get('/hello/{name}', 'Hello@show');
