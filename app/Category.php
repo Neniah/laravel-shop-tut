85849997f2cba2b875b778c0ac46f82d2ae7ea45
Maria Lobillo <maria.lobillo.santos@gmail.com>
2016-10-12 13:22:24 +0200

    fillable for categories

--- app/Category.php
+++ app/Category.php
@@ -9,4 +9,5 @@ class Category extends Model
     //
     protected $primaryKey = 'id';
     protected $table = 'categories';
+    protected $fillable = ['name', 'created_at_ip', 'updated_at_ip'];
 }
